3e9bf3b5088db272b25a2011379b7509e396582b
mallen <coolxbin@gmail.com>
2014-03-27 16:34:16 +0800

    测试sourcebean中的getCreateTime方法返回String类型，targetbean是否能够通过一个参数为String的方法将string类型的值转换为date类型。

--- mallen-beancopy/src/test/java/com/huangzb/test/bean/SourceBean.java
+++ mallen-beancopy/src/test/java/com/huangzb/test/bean/SourceBean.java
@@ -1,10 +1,13 @@
 package com.huangzb.test.bean;
 
+import java.util.Date;
+
 public class SourceBean {
 	private String name;
 	private int age;
 	private String sex;
 	private SourceInnerBean innerBean;
+	private Date createTime;
 
 	public SourceBean(String name, int age) {
 		super();
@@ -42,6 +45,18 @@ public class SourceBean {
 
 	public void setInnerBean(SourceInnerBean innerBean) {
 		this.innerBean = innerBean;
+	}
+// 这儿是不能定义返回值为String的，因为target中的是date，copier不会来调用该方法
+//	public String getCreateTime() {
+//		return  DateUtil.dateTimeToStr(createTime);
+//	}
+	
+	public Date getCreateTime() {
+		return createTime;
+	}
+	
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
 	}
 
 	@Override
--- mallen-beancopy/src/test/java/com/huangzb/test/bean/TargetBean.java
+++ mallen-beancopy/src/test/java/com/huangzb/test/bean/TargetBean.java
@@ -1,15 +1,33 @@
 package com.huangzb.test.bean;
 
+import java.util.Date;
+
+import com.huangzb.test.beancoper.DateUtil;
+
 public class TargetBean {
 	private String name;
 	private int age;
 	private String address;
 	private TargetInnerBean innerBean;
 	private SourceInnerBean sib;
+	private Date createTime;
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 	
+	public void setCreateTime(String createTime) {
+		this.createTime = DateUtil.strToDateTime(createTime);
+	}
+
 	public String toString() {
 		return "TargetBean:name = " + name + ", age = " + age + ", address = "
-				+ address + ", innerBean = " +innerBean + ", sib = " + sib;
+				+ address + ", innerBean = " + innerBean + ", sib = " + sib
+				+ " createTime=" + createTime;
 	}
 
 	public TargetBean() {
