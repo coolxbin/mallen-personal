7910d9c4dd9a28d118768c1ab4d22cdeb3c8a921
mallen <coolxbin@gmail.com>
2014-10-29 22:41:00 +0800

    添加<jsp:include page="">的测试代码，并修改struts2-web项目配置，以使其能满足启动需求

---
+++ mallen-struts2-web/src/main/java/com/huangzb/test/web/action/JspIncludeTestAction.java
@@ -0,0 +1,51 @@
+package com.huangzb.test.web.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller("jspIncludeTestAction")
+@Namespace("/jspinclude")
+@ResultPath("/views")
+@ParentPackage("json-default")
+@Scope(value = "prototype")
+public class JspIncludeTestAction extends ActionSupport{
+	private static final long serialVersionUID = -3792805743270963317L;
+	private String path;
+	private Map<String, String> entityMap;
+	@Action(value="test", results={@Result(name="success",location="JspIncludeTest.jsp")})
+	public String test(){
+		path = "innerPage2.jsp";
+		entityMap = new HashMap<String, String>();
+		entityMap.put("key1", "hello");
+		entityMap.put("key2", " ,world");
+		return SUCCESS;
+	}
+	
+	public Map<String, String> getEntityMap() {
+		return entityMap;
+	}
+
+
+	public void setEntityMap(Map<String, String> entityMap) {
+		this.entityMap = entityMap;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
